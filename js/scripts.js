2f5d004d68fedc99cfbb90a78ac5f68b2719b57d
Sage Sypert <sage.sypert@gmail.com>
2021-01-15 15:04:05 -0800

    add more else if statements to scripts page to manipulate quiz results

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,15 @@ $(document).ready(function() {
     let subject = $("input[name='subject']:checked").val();
     let attire = $("input[name='attire']:checked").val();
 
-    if (experience === '')
+    if (experience === 'yes' && company === 'corporate' && phone === 'android' && subject === 'math' && attire === 'thursday') {
+      $("#csharp").show();
+    } else if (experience === 'no' && company === 'startup' && phone === 'iphone' && subject === 'english' && attire === 'friday') {
+      $("#ruby").show();
+    } else if (experience === 'yes' && company === 'corporate' && phone === 'iphone' && subject === 'math' && attire === 'friday') {
+      $("javascript").show();
+    }
 
-
+    
+    event.preventDefault();
   });
 });
