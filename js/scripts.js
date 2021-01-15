24221ecf15bade36e0f947b88b7803c4d733267b
Sage Sypert <sage.sypert@gmail.com>
2021-01-15 14:53:26 -0800

    add new variable format to scripts page for radio buttons as well as try new if and statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
   $("form#programming-quiz").submit(function(event) {
-  
+    
+    let experience = $("input[name='experience']:checked").val();
+    let company = $("input[name='company']:checked").val();
+    let phone = $("input[name='phone']:checked").val();
+    let subject = $("input[name='subject']:checked").val();
+    let attire = $("input[name='attire']:checked").val();
+
+    if (experience === '')
+
+
   });
 });
