bc324e16b2b84cc88c4cc38156bc9012bb7c98f9
Sage Sypert <sage.sypert@gmail.com>
2021-01-15 14:09:31 -0800

    delete all drop down form, replace with commented out radio buttons instead to see if its easier to calculate result

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,5 @@
 $(document).ready(function() {
   $("form#programming-quiz").submit(function(event) {
 
-    const experience = true;
-    const company = true;
-    const phone = true;
-    const subject = true;
-    const attire = true;
-    
-    let result = true
-    
-    if (experience && company && phone && subject && attire == "true") {
-      $("#csharp").show();
-    }
-    event.preventDefault();
   });
 });
