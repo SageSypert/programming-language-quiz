9849e4069a03d3af15f3afd9a3801803ad3f58d2
Sage Sypert <sage.sypert@gmail.com>
2021-01-16 21:01:13 -0800

    add new if, else if, and else statements to get quiz to properly show/hide quiz results

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,52 @@
 $(document).ready(function() {
   $("form#programming-quiz").submit(function(event) {
-    
-    const experience = $("input[name='experience']:checked").val();
-    const company = $("input[name='company']:checked").val();
-    const phone = $("input[name='phone']:checked").val();
-    const subject = $("input[name='subject']:checked").val();
-    const attire = $("input[name='attire']:checked").val();
 
-    if (experience === 'yes' && company === 'corporate' && phone === 'android' && subject === 'math' && attire === 'thursday') {
+    const experience = $("input:radio[name='experience']:checked").val();
+    const company = $("input:radio[name='company']:checked").val();
+    const phone = $("input:radio[name='phone']:checked").val();
+    const subject = $("input:radio[name='subject']:checked").val();
+    const attire = $("input:radio[name='attire']:checked").val();
+
+    if (experience === "yes") {
       $("#csharp").show();
-    } else if (experience === 'no' && company === 'startup' && phone === 'iphone' && subject === 'english' && attire === 'friday') {
+    } else if (experience === "no") {
       $("#ruby").show();
-    } else if (experience === 'yes' && company === 'corporate' && phone === 'iphone' && subject === 'math' && attire === 'friday') {
-      $("javascript").show();
+    } else {
+      $("#javascript").show();
     }
 
+    if (company === "corporate") {
+      $("#csharp").show();
+    } else if (company === "startup") {
+      $("#ruby").show();
+    } else {
+      $("#javascript").show()
+    }
+
+    if (phone === "iphone") {
+      $("#ruby").show();
+    } else if (phone === "android") {
+      $("#csharp").show();
+    } else {
+      $("#javascript").show();
+    }
+
+    if (subject === "english") {
+      $("#ruby").show();
+    } else if (subject === "math") {
+      $("#javascript").show();
+    } else {
+      $("#csharp").show();
+    }
+
+    if (attire === "thursday") {
+      $("#csharp").show();
+    } else if (attire === "friday") {
+      $("#ruby").show();
+    } else {
+      $("#javascript").show();
+    }
+    
     event.preventDefault();
   });
 });
