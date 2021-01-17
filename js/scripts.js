fa7633e76c5ebbace8280d8237dc05c9443b1b45
Sage Sypert <sage.sypert@gmail.com>
2021-01-16 21:15:39 -0800

    add div class to each quiz result

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,32 @@
 $(document).ready(function() {
   $("form#programming-quiz").submit(function(event) {
 
-    const experience = $("input:radio[name='experience']:checked").val();
-    const company = $("input:radio[name='company']:checked").val();
-    const phone = $("input:radio[name='phone']:checked").val();
-    const subject = $("input:radio[name='subject']:checked").val();
-    const attire = $("input:radio[name='attire']:checked").val();
+    const experience = $("input:radio[name=experience]:checked").val();
+    const company = $("input:radio[name=company]:checked").val();
+    const phone = $("input:radio[name=phone]:checked").val();
+    const subject = $("input:radio[name=subject]:checked").val();
+    const attire = $("input:radio[name=attire]:checked").val();
 
     if (experience === "yes") {
       $("#csharp").show();
+      document.getElementById("csharp").style.display = "none";
     } else if (experience === "no") {
       $("#ruby").show();
+      document.getElementById("ruby").style.display = "none";
     } else {
       $("#javascript").show();
+      document.getElementById("javascript").style.display = "none";
     }
 
     if (company === "corporate") {
       $("#csharp").show();
+      document.getElementById("csharp").style.display = "none";
     } else if (company === "startup") {
       $("#ruby").show();
+      document.getElementById("ruby").style.display = "none";
     } else {
       $("#javascript").show()
+      document.getElementById("javascript").style.display = "none";
     }
 
     if (phone === "iphone") {
@@ -46,7 +52,7 @@ $(document).ready(function() {
     } else {
       $("#javascript").show();
     }
-    
+
     event.preventDefault();
   });
 });
