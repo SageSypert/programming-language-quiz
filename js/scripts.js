d3aeb6ab03d0c8fe8616af0bc3168e4afef919fb
Sage Sypert <sage.sypert@gmail.com>
2021-01-20 12:37:34 -0800

    change values to weight based to calculate results since otherwise was not working properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,66 @@
 $(document).ready(function() {
-  $("form#programmingquiz").submit(function(event) {
-    
-    const experience = $("input:radio[name=experience]:checked").val();
-    const company = $("input:radio[name=company]:checked").val();
-    const phone = $("input:radio[name=phone]:checked").val();
-    const subject = $("input:radio[name=subject]:checked").val();
-    const attire = $("input:radio[name=attire]:checked").val();
+$("form#programmingquiz").submit(function(event) {
+  $("#javascript").hide();
+  $("#csharp").hide();
+  $("#ruby").hide();
+  $("#no").hide();
 
-    if (experience === "yes") {
-      $("#csharp").show();
-    } else if (experience === "no") {
-      $("#ruby").show();
-    } else if (experience === "some") {
-      $("#javascript").show();
-    }
+  event.preventDefault();
+  
+  const experience = parseInt($("input:radio[name=experience]:checked").val());
+  const company = parseInt($("input:radio[name=company]:checked").val());
+  const phone = parseInt($("input:radio[name=phone]:checked").val());
+  const subject = parseInt($("input:radio[name=subject]:checked").val());
+  const attire = parseInt($("input:radio[name=attire]:checked").val());
 
-    if (company === "corporate") {
-      $("#csharp").show();
-    } else if (company === "startup") {
-      $("#ruby").show();
-    } else if (company === "mine") {
-      $("#javascript").show();
-    }
+  if (experience + company + phone + subject + attire === 10) {
+    $("#csharp").show();
+  } else if (experience + company + phone + subject + attire === 9) {
+    $("#javascript").show();
+  } else if (experience + company + phone + subject + attire === 8) {
+    $("#javascript").show();
+  } else if (experience + company + phone + subject + attire === 7) {
+    $("#ruby").show();
+  } else if (experience + company + phone + subject + attire === 6) {
+    $("#ruby").show();
+  } else if (experience + company + phone + subject + attire === 5) {
+    $("#ruby").show();
+  }
 
-    if (phone === "iphone") {
-      $("#ruby").show();
-    } else if (phone === "android") {
-      $("#csharp").show();
-    } else if (phone === "neither") {
-      $("#javascript").show();
-    }
+  // if (company === "corporate") {
+  //   $("#csharp").show();
+  // } else if (company === "startup") {
+  //   $("#ruby").show();
+  // } else if (company === "mine") {
+  //   $("#javascript").show();
+  // }
 
-    if (subject === "english") {
-      $("#ruby").show();
-    } else if (subject === "math") {
-      $("#csharp").show();
-    } else if (subject === "science") {
-      $("#javascript").show();
-    }
+  // if (phone === "iphone") {
+  //   $("#ruby").show();
+  // } else if (phone === "android") {
+  //   $("#csharp").show();
+  // } else if (phone === "neither") {
+  //   $("#javascript").show();
+  // }
 
-    if (attire === "thursday") {
-      $("#csharp").show();
-    } else if (attire === "friday") {
-      $("#ruby").show();
-    } else  if (attire === "everyday") {
-      $("#javascript").show();
-    }
+  // if (subject === "english") {
+  //   $("#ruby").show();
+  // } else if (subject === "math") {
+  //   $("#csharp").show();
+  // } else if (subject === "science") {
+  //   $("#javascript").show();
+  // }
 
-    event.preventDefault();
+  // if (attire === "thursday") {
+  //   $("#csharp").show();
+  // } else if (attire === "friday") {
+  //   $("#ruby").show();
+  // } else  if (attire === "everyday") {
+  //   $("#javascript").show();
+  // }
 
     $("#reset").click(function(){
-      document.location.reload(true);
+    document.location.reload(true);
     });
   });
 });
